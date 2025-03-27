ade81a33699fd56f627968e7fcaf8869f85bbcfe
Juargo <jorgeretamal.jjra@gmail.com>
2025-03-26 22:45:32 -0300

    chore: Add new files for release

---
+++ backend/setup.py
@@ -0,0 +1,12 @@
+"""Setup script for the backend package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="moneydiary-backend",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Backend for Money Diary application",
+    author="",
+    author_email="",
+)
---
+++ backend/version.py
@@ -0,0 +1,3 @@
+"""Version of the backend package."""
+
+__version__ = "0.1.0"
