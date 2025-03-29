ffcf25fa2d132068413726d0f830e6644ed67cbb
Juargo <jorgeretamal.jjra@gmail.com>
2025-03-29 14:17:47 -0300

    feat(config): agregar prefijo '/moneydiary' a todas las URLs generadas

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcssPostcss from '@tailwindcss/postcss';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  base: '/moneydiary', // Añadimos este prefijo para todas las URLs generadas
   vite: {
     plugins: [],
     css: {
